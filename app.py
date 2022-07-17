15ec311ab92d231df0cad14b5d0ea0bacc2a087e
Kaydarin <zim50_killerz@yahoo.com>
2022-07-18 00:25:58 +0800

    Created function to decode authorization header token.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, url_for, g
 import requests
 import jwt
 from datetime import datetime, timedelta
@@ -10,8 +10,32 @@ GITHUB_CLIENT_SECRET = ""
 app = Flask(__name__)
 
 
+@app.before_request
+def before_request():
+    if (
+        request.endpoint != "hello_world"
+        and request.endpoint != "login"
+        and request.endpoint != "login_callback"
+    ):
+        auth_token = request.headers["Authorization"]
+        token = auth_token.split("Bearer ")[1]
+
+        try:
+            decoded = jwt.decode(token, JWT_KEY, algorithms=["HS256"])
+            g.user = decoded["id"]
+        except jwt.ExpiredSignatureError:
+            return ("Expired", 401)
+        except jwt.InvalidSignatureError:
+            return ("Invalid", 401)
+
+
 @app.route("/")
 def hello_world():
+    return redirect(url_for("login"))
+
+
+@app.route("/login")
+def login():
     return redirect(
         "https://github.com/login/oauth/authorize?client_id=" + GITHUB_CLIENT_ID,
         code=302,
@@ -61,7 +85,7 @@ def login_callback():
             {"id": user_data["id"], "exp": expiry}, JWT_KEY, algorithm="HS256"
         )
 
-        return encoded
+        return ("Login success", {"Authorization": "Bearer " + encoded})
 
     else:
-        return ("Login failed", 400)
+        return ("Login failed", 500)
