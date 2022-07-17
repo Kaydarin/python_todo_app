eab1948905955e32086098e777fa5f285489df57
Kaydarin <zim50_killerz@yahoo.com>
2022-07-17 23:45:33 +0800

    Added GitHub user info request on login. Implemented JWT.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, redirect
 import requests
+import jwt
+from datetime import datetime, timedelta
 
+JWT_KEY = ""
 GITHUB_CLIENT_ID = ""
 GITHUB_CLIENT_SECRET = ""
 
@@ -35,7 +38,30 @@ def login_callback():
 
         data = res.json()
 
-        return "Login success"
+        res = requests.get(
+            "https://api.github.com/user",
+            headers={
+                "Accept": "application/vnd.github+json",
+                "Authorization": "token " + data["access_token"],
+            },
+        )
+
+        if res.ok is not True:
+            return ("Login failed", 500)
+
+        user_data = res.json()
+
+        # user_data['id'] # login id
+        # user_data['login'] # login name
+        # user_data['html_url'] # user repo url
+
+        expiry = (datetime.now() + timedelta(hours=8)).timestamp()
+
+        encoded = jwt.encode(
+            {"id": user_data["id"], "exp": expiry}, JWT_KEY, algorithm="HS256"
+        )
+
+        return encoded
 
     else:
         return ("Login failed", 400)
