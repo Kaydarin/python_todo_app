9393bb424d749b01746e4388d20e990f7445a5b2
Kaydarin <zim50_killerz@yahoo.com>
2022-07-18 20:05:57 +0800

    Integrated SQLAlchemy ORM with MySQL dialect. Created User database model. Added function create user on login.

--- app.py
+++ app.py
@@ -2,12 +2,35 @@ from flask import Flask, request, redirect, url_for, g
 import requests
 import jwt
 from datetime import datetime, timedelta
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 JWT_KEY = ""
 GITHUB_CLIENT_ID = ""
 GITHUB_CLIENT_SECRET = ""
+MYSQL_CONN = ""
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = MYSQL_CONN
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    github_id = db.Column(db.String(128), unique=True, nullable=False)
+    github_username = db.Column(db.String(255), nullable=False)
+    github_userurl = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime)
+    deleted_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return "<User %r>" % self.github_username
 
 
 @app.before_request
@@ -22,7 +45,7 @@ def before_request():
 
         try:
             decoded = jwt.decode(token, JWT_KEY, algorithms=["HS256"])
-            g.user = decoded["id"]
+            g.user_id = decoded["id"]
         except jwt.ExpiredSignatureError:
             return ("Expired", 401)
         except jwt.InvalidSignatureError:
@@ -75,17 +98,33 @@ def login_callback():
 
         user_data = res.json()
 
+        user = User.query.filter_by(github_id=user_data["id"]).first()
+
+        if user is None:
+            new_user = User(
+                github_id=user_data["id"],
+                github_username=user_data["login"],
+                github_userurl=user_data["html_url"],
+                created_at=func.now(),
+            )
+
+            db.session.add(new_user)
+            db.session.commit()
+            db.session.refresh(new_user)
+            user = new_user
+
         # user_data['id'] # login id
         # user_data['login'] # login name
         # user_data['html_url'] # user repo url
 
         expiry = (datetime.now() + timedelta(hours=8)).timestamp()
 
-        encoded = jwt.encode(
-            {"id": user_data["id"], "exp": expiry}, JWT_KEY, algorithm="HS256"
-        )
+        encoded = jwt.encode({"id": user.id, "exp": expiry}, JWT_KEY, algorithm="HS256")
 
-        return ("Login success", {"Authorization": "Bearer " + encoded})
+        return (
+            {"message": "Login success", "token": encoded},
+            {"Authorization": "Bearer " + encoded},
+        )
 
     else:
         return ("Login failed", 500)
