d5b3d932d898836db2403f237dd5738d21ac5330
Kaydarin <zim50_killerz@yahoo.com>
2022-07-19 22:32:25 +0800

    Created update & delete API for Todo.

--- app.py
+++ app.py
@@ -416,6 +416,86 @@ def add_todo():
     return ({"message": "Successfully add new todo!", "id": new_todo.id}, 200)
 
 
+@app.patch("/update-todo")
+def update_todo():
+    data = request.form
+
+    todo_id = data.get("id", None)
+    title = data.get("title", None)
+
+    if todo_id is None or title is None:
+        return ("Missing attribute", 422)
+
+    todo = (
+        Todo.query.with_entities(Todo.id, Todo.task_id)
+        .join(Task, Task.id == Todo.task_id)
+        .filter(
+            Todo.id == todo_id,
+            Task.user_id == g.user_id,
+            Task.deleted_at == None,
+            Todo.deleted_at == None,
+        )
+        .one_or_none()
+    )
+
+    if todo is None:
+        return ({"message": "Todo is not exist"}, 400)
+
+    data_to_update = {"title": title, "updated_at": func.now()}
+
+    updated = (
+        db.session.query(Todo)
+        .filter(
+            Todo.id == todo_id,
+            Todo.deleted_at == None,
+        )
+        .update(data_to_update)
+    )
+    db.session.commit()
+
+    if updated == 1:
+        return ({"message": "Successfully updated todo!"}, 200)
+    else:
+        return ({"message": "Nothing updated"}, 202)
+
+
+@app.delete("/remove-todo")
+def delete_todo():
+    data = request.form
+
+    todo_id = data.get("id", None)
+
+    if todo_id is None:
+        return ("Missing attribute", 422)
+
+    todo = (
+        Todo.query.with_entities(Todo.id, Todo.task_id)
+        .join(Task, Task.id == Todo.task_id)
+        .filter(
+            Todo.id == todo_id,
+            Task.user_id == g.user_id,
+            Task.deleted_at == None,
+            Todo.deleted_at == None,
+        )
+        .one_or_none()
+    )
+
+    if todo is None:
+        return ({"message": "Todo is not exist"}, 400)
+
+    deleted = (
+        db.session.query(Todo)
+        .filter(Todo.id == todo_id, Todo.deleted_at == None)
+        .update({"deleted_at": func.now()})
+    )
+    db.session.commit()
+
+    if deleted == 1:
+        return ({"message": "Successfully delete todo!"}, 200)
+    else:
+        return ({"message": "Nothing deleted"}, 202)
+
+
 @app.get("/callback")
 def login_callback():
     code = request.args.get("code")
